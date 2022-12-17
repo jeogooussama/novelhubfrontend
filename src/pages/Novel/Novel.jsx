a1b825fd52020805418b9456ac06f29fe8c53f3e
jeogooussama <jeogodz@protonmail.com>
2022-12-17 10:25:37 +0100

    modified:   src/pages/Novel/Novel.jsx

--- src/pages/Novel/Novel.jsx
+++ src/pages/Novel/Novel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
